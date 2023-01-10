c8b8c23e29abd83a47e9d01fc5c3e63809f9332a
erina/eric robot <eric.mountain@gmail.com>
2023-01-10 21:08:21 +0100

    Store in deep archive

--- deep-freeze.py
+++ deep-freeze.py
@@ -29,8 +29,8 @@ class BackupProcessor():
         self.archive_sequence_nb = 0
         # TODO config
         self.archive_max_size_bytes = 200000000 # 200MB
-        #self.s3_storage_class = "DEEP_ARCHIVE"
-        self.s3_storage_class = "STANDARD"
+        self.s3_storage_class = "DEEP_ARCHIVE"
+        #self.s3_storage_class = "STANDARD"
 
     def run(self):
         self.db.prepare_backup()
