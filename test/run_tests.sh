ca1e944aca75e67c93643d90049d80065465da65
Eric Mountain <eric.mountain@gmail.com>
2023-01-17 20:55:52 +0100

    Add test run timing and prepare for larger scale testing

--- test/run_tests.sh
+++ test/run_tests.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 set -euo pipefail
-set -x
+#set -x
 
 [[ ! -d ~/.deep-freeze-backups ]] && mkdir ~/.deep-freeze-backups
 rm -f ~/.deep-freeze-backups/deep-freeze-backups.db
@@ -21,7 +21,23 @@ dd if=/dev/urandom of="${test_root}/a1/b1/c1/c1_2.dat" bs=1k count=2
 dd if=/dev/urandom of="${test_root}/d1/e1/f1/f1_1.dat" bs=1k count=2
 dd if=/dev/urandom of="${test_root}/d1/e2/f2/f2_1.dat" bs=1k count=2
 
-../deep-freeze.py
+#cd "${test_root}"
+#for i in $(seq 10) ; do
+#  mkdir $i
+#  cd $i
+#  for j in $(seq 10) ; do
+#    mkdir $j
+#    cd $j
+#    for k in $(seq 1000) ; do
+#      touch $k
+#    done
+#    cd ..
+#  done
+#  cd ..
+#done
+#cd /deep-freeze/test
+
+time ../deep-freeze.py
 
 # FIXME Ensure archive name changes thanks to timestamp
 sleep 1
@@ -29,11 +45,11 @@ sleep 1
 # Ensure the size is different so we detect a change
 dd if=/dev/urandom of="${test_root}/d1/e2/f2/f2_1.dat" bs=1k count=3
 
-../deep-freeze.py
+time ../deep-freeze.py
 
 rm "${test_root}/a1/b1/c1/c1_1.dat"
 
-../deep-freeze.py
+time ../deep-freeze.py
 
 sqlite3 ~/.deep-freeze-backups/deep-freeze-backups.db \
   ".headers on" "select * from files; select * from file_archive_records; select * from s3_archives;"
