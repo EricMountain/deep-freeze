0ced090ee2441e26d7be7f729d41bdb724b3a654
erina/eric robot <eric.mountain@gmail.com>
2023-01-19 22:12:45 +0100

    Perform disk scan in transaction
    Immense speedup for large filesystems.

--- db/file.py
+++ db/file.py
@@ -33,24 +33,23 @@ class File():
 
         datetime_str = self._epoch2fmt(self.metadata.st_mtime)
 
-        with self.db.connection:
-            cursor = self.db.connection.cursor()
-            query = '''
-                  insert into files(client_fqdn, backup_root, relative_path, size, modification, status, sweep_mark,
-                                    force_backup, new_size, new_modification, new_status)
-                  values(?,?,?,?,?,'present','present','Y',?,?,'present')
-                  on conflict(client_fqdn, backup_root, relative_path) do
-                  update set
-                      new_size = ?,
-                      new_modification = ?,
-                      new_status = 'present',
-                      sweep_mark = 'present'
-                  '''
-            cursor.execute(query, (self.client_config.client_fqdn, self.client_config.backup_root,
-                                   self.rel_path, self.metadata.st_size,
-                                   datetime_str,
-                                   self.metadata.st_size, datetime_str,
-                                   self.metadata.st_size, datetime_str))
+        cursor = self.db.connection.cursor()
+        query = '''
+                insert into files(client_fqdn, backup_root, relative_path, size, modification, status, sweep_mark,
+                                force_backup, new_size, new_modification, new_status)
+                values(?,?,?,?,?,'present','present','Y',?,?,'present')
+                on conflict(client_fqdn, backup_root, relative_path) do
+                update set
+                    new_size = ?,
+                    new_modification = ?,
+                    new_status = 'present',
+                    sweep_mark = 'present'
+                '''
+        cursor.execute(query, (self.client_config.client_fqdn, self.client_config.backup_root,
+                                self.rel_path, self.metadata.st_size,
+                                datetime_str,
+                                self.metadata.st_size, datetime_str,
+                                self.metadata.st_size, datetime_str))
 
     def _epoch2fmt(self, epoch) -> str:
         return time.strftime('%Y-%m-%d %H:%M:%S %Z', time.gmtime(epoch))
